2bba9fcb1a3c1bfae13097f81c7ecc435149dda7
Jo Schlemper <js3611@imperial.ac.uk>
2013-06-02 23:38:49 +0100

    Added comment to httpreaders.java

--- MoneyApp/src/com/example/helpers/HttpReaders.java
+++ MoneyApp/src/com/example/helpers/HttpReaders.java
@@ -6,16 +6,17 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.UnsupportedEncodingException;
 
+/* A class contains helper functions which converts InputStream data to the appropriate format */
 public class HttpReaders {
 
-	  
-		//Reads an InputStream and converts it to a String.
-		public static String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
-		   Reader reader = null;
-		   reader = new InputStreamReader(stream, "UTF-8");        
-		   char[] buffer = new char[len];
-		   reader.read(buffer);
-		   return new String(buffer);
-		}
-	
+	// Reads an InputStream and converts it to a String.
+	public static String readIt(InputStream stream, int len)
+			throws IOException, UnsupportedEncodingException {
+		Reader reader = null;
+		reader = new InputStreamReader(stream, "UTF-8");
+		char[] buffer = new char[len];
+		reader.read(buffer);
+		return new String(buffer);
+	}
+
 }
