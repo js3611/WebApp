94112b34c072b662bf386a0cec3d7fd533db1740
Thai Tam <thaitam.nguyen11@imperial.ac.uk>
2013-06-03 15:26:05 +0100

    Getting intent + limitations on SDK

--- MoneyApp/src/com/example/moneyapp/MainMenu.java
+++ MoneyApp/src/com/example/moneyapp/MainMenu.java
@@ -1,17 +1,27 @@
 package com.example.moneyapp;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
 public class MainMenu extends Activity {
 
+	@SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		Intent intent = getIntent();
 		setContentView(R.layout.activity_main_menu);
+		
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            // SDK must be over HONEYCOMB for the calendar to show up
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 	}
 
 	@Override
@@ -21,27 +31,27 @@ public class MainMenu extends Activity {
 		return true;
 	}
 
-	public void toCalendar(View view){
+	protected void toCalendar(View view){
 		Intent intent = new Intent(MainMenu.this, Calendar.class);
 		startActivity(intent);
 	}
 	
-	public void toTransactions(View view){
+	protected void toTransactions(View view){
 		Intent intent = new Intent(MainMenu.this, Transactions.class);
 		startActivity(intent);
 	}
 	
-	public void toMessages(View view){
+	protected void toMessages(View view){
 		Intent intent = new Intent(MainMenu.this, Messages.class);
 		startActivity(intent);
 	}
 	
-	public void toWishList(View view){
+	protected void toWishList(View view){
 		Intent intent = new Intent(MainMenu.this, WishList.class);
 		startActivity(intent);
 	}
 	
-	public void toSettings(View view){
+	protected void toSettings(View view){
 		Intent intent = new Intent(MainMenu.this, Settings.class);
 		startActivity(intent);
 	}
