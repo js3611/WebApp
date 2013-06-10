aa73240fbcfa6ac3bcc9fb9ce100d7c753c4defa
Jo Schlemper <js3611@glyph14.doc.ic.ac.uk>
2013-06-10 22:08:54 +0100

    all the errors in transaction solved

--- example-context/src/Transaction.java
+++ example-context/src/Transaction.java
@@ -1,235 +1,283 @@
 // Servlet_Postgres.java - example connection to Postgres
 import java.io.*;
+import java.math.BigDecimal;
 import java.text.*;
 import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+
 import java.sql.*;
 
+public class Transaction extends HttpServlet {
 
-public class Transaction extends HTTPservlet {
-
-
-	public void doGet(HttpServletRequest request,
-			HttpServletResponse response)
-					throws IOException, ServletException
-					{
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
 		PrintWriter out = response.getWriter();
 		String operation = request.getParameter("op");
 
-				if (operation == null) {
-					outprintln("br>no operation specified</br>");
-					return;
-				}
+		if (operation == null) {
+			out.println("br>no operation specified</br>");
+			return;
+		}
 
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e) {
-			out.println("<h1>Driver not found: " + e + e.getMessage() + "</h1>" );
-		}		
+			out.println("<h1>Driver not found: " + e + e.getMessage() + "</h1>");
+		}
 
 		try {
-			Connection conn = DriverManager.getConnection ("jdbc:postgresql://db.doc.ic.ac.uk/g1227132_u",
-					"g1227132_u", "W0zFGMaqup");	
+			Connection conn = DriverManager.getConnection(
+					"jdbc:postgresql://db.doc.ic.ac.uk/g1227132_u",
+					"g1227132_u", "W0zFGMaqup");
 			handleGetOperation(operation, conn, request, out);
 
 		} catch (Exception e) {
-			out.println( "<h1>exception: "+e+e.getMessage()+"</h1>" );
+			out.println("<h1>exception: " + e + e.getMessage() + "</h1>");
 		}
-					}
+	}
 
-
-	private void handleGetOperation(String operation, Connection conn, 
+	private void handleGetOperation(String operation, Connection conn,
 			HttpServletRequest request, PrintWriter out) throws Exception {
 
-		if (operation.equals("viewLiveTransactions"){
+		if (operation.equals("viewLiveTransactions")) {
 			Statement transStmt = conn.createStatement();
-			int userid = request.getParameter("userid");		
+			int userid = Integer.parseInt(request.getParameter("userid"));
 
-			ResultSet transactionsSet = transStmt.executeQuery("SELECT (t.transid, t.name, t._date, t.total_amount, d.owesuserid) FROM transactions t INNER JOIN debt d on t.transid = d.transid WHERE (t. userid = " + userid + " OR d.owesuserid = " + userid +") AND t.total_paid_off = false GROUP BY t.transid, t.name, t._date, t.total_amount, d.owesuserid ORDER BY t._date DESC");		
+			ResultSet transactionSet = transStmt
+					.executeQuery("SELECT (t.transid, t.name, t._date, t.total_amount, d.owesuserid) FROM transactions t INNER JOIN debt d on t.transid = d.transid WHERE (t. userid = "
+							+ userid
+							+ " OR d.owesuserid = "
+							+ userid
+							+ ") AND t.total_paid_off = false GROUP BY t.transid, t.name, t._date, t.total_amount, d.owesuserid ORDER BY t._date DESC");
 
 			if (!transactionSet.next()) {
-				out.print("4"); //NO CURRENT TRANSACTIONS
+				out.print("4"); // NO CURRENT TRANSACTIONS
 
 				while (transactionSet.next()) {
-					// TODO: So we got the transactions, now need to display them	
+					// TODO: So we got the transactions, now need to display
+					// them
+				}
+			}
+		} else if (operation.equals("viewDeadTransactions")) {
+			Statement transStmt = conn.createStatement();
+			int userid = Integer.parseInt(request.getParameter("userid"));
+
+			ResultSet transactionSet = transStmt
+					.executeQuery("SELECT (t.transid, t._date, t.name, t.total_amount, t.complete_date, d.userid, d.owesuserid) FROM transactions t INNER JOIN debt d on t.transid = d.transid WHERE (t. userid = "
+							+ userid
+							+ " OR d.owesuserid = "
+							+ userid
+							+ ") AND t.total_paid_off = true GROUP BY t.transid, t.desciption, t._date, t.name, t.total_amount, t.complete_date, d.userid, d.owesuserid ORDER BY t._date DESC");
+
+			if (!transactionSet.next()) {
+				out.print("4"); // NO CURRENT TRANSACTIONS
+
+				while (transactionSet.next()) {
+					// TODO: So we got the transactions, now need to display
+					// them
 				}
 
-			} else if (operation.equals("viewDeadTransactions") {
-				Statement transStmt = conn.createStatement();
-				int userid = request.getParameter("userid");		
+			} else if (operation.equals("transactionLiveDetails")) {
+				Statement detailsStmt = conn.createStatement();
+				Statement debtStmt = conn.createStatement();
 
-				ResultSet transactionsSet = transStmt.executeQuery("SELECT (t.transid, t._date, t.name, t.total_amount, t.complete_date, d.userid, d.owesuserid) FROM transactions t INNER JOIN debt d on t.transid = d.transid WHERE (t. userid = " + userid + " OR d.owesuserid = " + userid +") AND t.total_paid_off = true GROUP BY t.transid, t.desciption, t._date, t.name, t.total_amount, t.complete_date, d.userid, d.owesuserid ORDER BY t._date DESC");	
+				// TODO Currently just gets the transactions in which the user
+				// is the one who is owed (not owes)
 
-				if (!transactionSet.next()) {
-					out.print("4"); //NO CURRENT TRANSACTIONS
+				int transid = Integer.parseInt(request.getParameter("transid"));
+				ResultSet detailsSet = detailsStmt
+						.executeQuery("SELECT (transid, name, total_amount, _date, description, total_paid_off) FROM transactions WHERE transid = "
+								+ transid + " AND total_paid_off = false;");
 
-					while (transactionSet.next()) {
-						// TODO: So we got the transactions, now need to display them		
-					}
+				ResultSet rs = debtStmt
+						.executeQuery("SELECT (d.transid, d.amount, a.userid, a.firstname, a.surname) FROM debt d INNER JOIN appuser a ON d.owesuserid = appuser.userid");
 
-				} else if (operation.equals("transactionLiveDetails") {
-					Statement detailsStmt = conn.createStatement();
-					Statement debtStmt = conon.createStatement();
+				if (!rs.next())
+					out.print("5");
+				while (rs.next())
+					out.println(rs.getInt("transid") + ", "
+							+ rs.getInt("amount") + ", " + rs.getInt("userid")
+							+ ", " + rs.getInt("firstname") + ", "
+							+ rs.getString("firstname") + ", "
+							+ rs.getString("Surname"));
 
-					//TODO Currently just gets the transactions in which the user is the one who is owed (not owes)
+			} else if (operation.equals("transactionDeadDetails")) {
+				Statement detailsStmt = conn.createStatement();
 
-					int transid = request.getparameter("transid");
-					ResultSet detailsSet = detailsStmt.executeQuery("SELECT (transid, name, total_amount, _date, description, total_paid_off) FROM transactions WHERE transid = " + transid + " AND total_paid_off = false;");
+				int transid = Integer.parseInt(request.getParameter("transid"));
+				ResultSet detailsSet = detailsStmt
+						.executeQuery("SELECT (transid, name, total_amount, _date, description, total_paid_off) FROM transactions WHERE transid = "
+								+ transid + " AND total_paid_off = true;");
 
-					ResultSet rs = debtStmt.executeQuery("SELECT (d.transid, d.amount, a.userid, a.firstname, a.surname) FROM debt d INNER JOIN appuser a ON d.owesuserid = appuser.userid");
+			}
+		}
+	}
 
-					if (!rs.next())
-						out.print("5");
-					while(rs.next())
-						out.println(rs.getInt("transid") + ", " + rs.getInt("amount") + ", " + rs.getInt("userid") + ", " + rs.getInt("firstname") + ", " + rs.getString("firstname") + ", " + rs.getString("Surname")); 
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		PrintWriter out = response.getWriter();
+		String operation = request.getParameter("op");
 
+		if (operation == null) {
+			out.println("br>no operation specified</br>");
+			return;
+		}
 
-				} else if (operation.equals("transactionDeadDetails"){
-					Statement detailsStmt = conn.createStatement();
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			out.println("<h1>Driver not found: " + e + e.getMessage() + "</h1>");
+		}
 
-					int transid = request.getparameter("transid");
-					ResultSet detailsSet = detailsStmt.executeQuery("SELECT (transid, name, total_amount, _date, description, total_paid_off) FROM transactions WHERE transid = " + transid + " AND total_paid_off = true;");
+		try {
+			Connection conn = DriverManager.getConnection(
+					"jdbc:postgresql://db.doc.ic.ac.uk/g1227132_u",
+					"g1227132_u", "W0zFGMaqup");
+			handlePostOperation(operation, conn, request, out);
 
-				}
+		} catch (Exception e) {
+			out.println("<h1>exception: " + e + e.getMessage() + "</h1>");
+		}
 
+	}
 
+	private void handlePostOperation(String operation, Connection conn,
+			HttpServletRequest request, PrintWriter out) throws Exception {
 
+		// ADDS A NEW TRANSACTION TO THE DATABASE.
+		if (operation.equals("newTransaction")) {
+			// Add to transactions table
+			Statement transStmt = conn.createStatement();
 
+			int user_id = Integer.parseInt(request.getParameter("userid"));
+			String trans_name = request.getParameter("name");
+			String trans_desc = request.getParameter("desc");
+			int trans_date = Integer.parseInt(request.getParameter("date"));
+			BigDecimal trans_amount = new BigDecimal(request.getParameter(
+					"total_amount").replaceAll(",", ""));
+			int trans_urgency = Integer.parseInt(request
+					.getParameter("urgency"));
 
+			// Should return only 1 value
+			ResultSet new_trans = transStmt
+					.executeQuery("INSERT INTO transactions(name, description, _date, total_amount, urgency) values ('"
+							+ trans_name
+							+ "', '"
+							+ trans_desc
+							+ "', '"
+							+ trans_date
+							+ "', '£"
+							+ trans_amount
+							+ "', "
+							+ trans_urgency + ") RETURNING transid;");
+			// The transaction id of just added transaction above
+			int trans_id = new_trans.getInt("transid");
+			transStmt.close();
 
-				public void doPost(HttpServletRequest request,
-						HttpServletResponse response)
-								throws IOException, ServletException
-								{
-					PrintWriter out = response.getWriter();
-					Stringoperation = request.getParameter("op")
+			// Add to debt table
+			List<Pair<String, BigDecimal>> trans_owersList = null;// request.getParameter("owersList");
+			// Iterator owersIterator = trans_owersList.iterator();
 
-							if (operation == null) {
-								outprintln("br>no operation specified</br>");
-								return;
-							}
+			Statement insertStmt = conn.createStatement();
+			Statement queryStmt = conn.createStatement();
 
-					try {
-						Class.forName("org.postgresql.Driver");
-					} catch (ClassNotFoundException e) {
-						out.println("<h1>Driver not found: " + e + e.getMessage() + "</h1>" );
-					}		
+			for (Pair<String, BigDecimal> ower : trans_owersList) {
+				String ower_phone = ower.first;
+				BigDecimal ower_amount = ower.second;
+				ResultSet rs = queryStmt
+						.executeQuery("SELECT * FROM appuser WHERE phonenumber = '"
+								+ ower_phone + "';");
+				int owers_id = rs.getInt("userid");
+				int result = insertStmt
+						.executeUpdate("INSERT INTO debt(transid, userid, owesuserid, amount) values("
+								+ trans_id
+								+ ", "
+								+ user_id
+								+ ", "
+								+ owers_id
+								+ ", " + "'£" + ower_amount + "');");
 
-					try {
-						Connection conn = DriverManager.getConnection ("jdbc:postgresql://db.doc.ic.ac.uk/g1227132_u",
-								"g1227132_u", "W0zFGMaqup");	
-						handlePostOperation(operation, conn, request, out);
+				if (result == 1)
+					out.print("1"); // SUCCESSFUL ADD
+				else
+					out.print("2"); // DATABASE INSERT WENT WRONG
+			}
+			queryStmt.close();
+			insertStmt.close();
 
-					} catch (Exception e) {
-						out.println( "<h1>exception: "+e+e.getMessage()+"</h1>" );
-					}
+			// WHEN THE USER WANTS TO EDIT A TRANSACTION, E.G, A MISTAKE
+		} else if (operation.equals("updateTransaction")) {
 
-								}
+			// WHEN A PART OF THE DEBT HAS BEEN PAID BACK
+		} else if (operation.equals("debtRepaid")) {
+			Statement updateStmt = conn.createStatement();
+			Statement checkStmt = conn.createStatement();
 
+			int transid = Integer.parseInt(request.getParameter("transid"));
+			int userid = Integer.parseInt(request.getParameter("userid"));
+			int owesuserid = Integer.parseInt(request
+					.getParameter("owesuserid"));
 
-				private void handlePostOperation(String operation, Connection conn, 
-						HttpServletRequest request, PrintWriter out) throws Exception {
+			int rs = updateStmt
+					.executeUpdate("UPDATE debt SET paid_off = true WHERE transid = "
+							+ transid
+							+ " AND userid = "
+							+ userid
+							+ " AND owesuserid = " + owesuserid + ";");
 
-					// ADDS A NEW TRANSACTION TO THE DATABASE.
-					if (operation.equals("newTransaction")) {
-						//Add to transactions table
-						Statement transStmt = conn.createStatment();
+			if (rs != 0)
+				out.print("8");
+			else
+				out.print("9");
 
-						int user_id = request.getparameter("userid");
-						String trans_name = request.getParameter("name");
-						String trans_desc = request.getParameter("desc");
-						trans_date = request.getParameter("date");
-						BigDecimal trans_amount = request.getParameter("total_amount");
-						int trans_urgency = request.getParameter("urgency");	
+			ResultSet results = checkStmt
+					.executeQuery("SELECT * FROM debt WHERE transid = "
+							+ transid + ";");
 
-						//Should return only 1 value
-						ResultSet new_trans = transStmt.executeQuery("INSERT INTO transactions(name, description, _date, total_amount, urgency) values ('" + trans_name + "', '" + trans_desc + "', '" + trans_date +"', '£" + trans_amount + "', " + trans_urgency + ") RETURNING transid;");
-						// The transaction id of just added transaction above
-						int trans_id = new_trans.getInt("transid");
-						transStmt.close();
+			if (!results.next()) { // NO RESULTS SO ALL DEBTS HAVE BEEN PAID
+				rs = updateStmt
+						.executeUpdate("UPDATE transactions SET paid_off = true WHERE transid = "
+								+ transid + ";");
+				if (rs == 0)
+					out.print("11"); // statement didnt work
 
-						//Add to debt table
-						List<Pair<String,BigDecimal>> trans_owersList = request.getParameter("owersList");
-						//Iterator owersIterator = trans_owersList.iterator();
+			}
 
-						Statement insertStmt = conn.createStatement();
-						Statement queryStmt = conn.createStatement();
+			// WHEN THE USER WANTS TO DELETE A TRANSACTION
+		} else if (operation.equals("deleteTransaction")) {
+			Statement dltStmt = conn.createStatement();
+			Statement queryStmt = conn.createStatement();
 
+			int transid = Integer.parseInt(request.getParameter("transid"));
+			ResultSet rs = queryStmt
+					.executeQuery("SELECT userid FROM transactions WHERE transid = '"
+							+ transid + "';");
 
-						for (Pair<String, BigDecimal> ower : trans_owersList) {
-							String ower_phone = ower.first;
-							BigDecimal ower_amount = ower.second;
-							ResultSet rs = queryStmt.executeQuery("SELECT * FROM appuser WHERE phonenumber = '" + ower_phone +"';");
-							int owers_id = rs.getString("userid"); 		
-							int result = insertStmt.executeUpdate("INSERT INTO debt(transid, userid, owesuserid, amount) values(" + trans_id + ", " + user_id + ", " + owers_id + ", " + "'£"+ ower_amount + "');");  
-						}
-						queryStmt.close();
-						insertStmt.close();
+			int userid = Integer.parseInt(request.getParameter("userid"));
+			int useridcheck = rs.getInt("userid");
 
-						if (result == 1)
-							out.print("1");	 // SUCCESSFUL ADD
-						else
-							out.print("2");   // DATABASE INSERT WENT WRONG
+			int result = dltStmt
+					.executeUpdate("DELETE FROM transactions WHERE transid = "
+							+ transid + ";");
+			if (result != 0)
+				out.print("10"); // User is the person who is owed money
+			else
+				out.print("11"); // The DELETE statement didnt execute correctly
 
+			dltStmt.close();
+			queryStmt.close();
 
+		} else
+			out.print("2"); // COULD NOT RECOGNISE OPERATION
 
-						// WHEN THE USER WANTS TO EDIT A TRANSACTION, E.G, A MISTAKE
-					}  else if (operation.equals("updateTransaction")) {
+	}
 
+	class Pair<S, T> {
 
-						// WHEN A PART OF THE DEBT HAS BEEN PAID BACK
-					}  else if (operation.equals("debtRepaid")) {
-						Statement updateStmt = conn.createStatement();
-						Statement checkStmt = conn.createStatement();
+		public BigDecimal second;
+		public String first;
 
-						int transid = request.getParameter("transid");
-						int userid = request.getParameter("userid");
-						int owesuserid = request.getparameter("owesuserid");
+	}
 
-						int rs = updateStmt.executeUpdate("UPDATE debt SET paid_off = true WHERE transid = " + transid + " AND userid = " + userid + " AND owesuserid = " + owesuserid + ";");
-
-						if (rs != 0)
-							out.print("8");
-						else
-							out.print("9");
-
-						ResultSet results = checkStmt("SELECT * FROM debt WHERE transid = "+transid+";");
-
-						if (!results.next()) {	//NO RESULTS SO ALL DEBTS HAVE BEEN PAID
-							rs = updateStmt.executeUpdate("UPDATE transactions SET paid_off = true WHERE transid = " + transid +";");
-							if (rs == 0)
-								out.print("11"); //statement didnt work
-
-
-
-							// WHEN THE USER WANTS TO DELETE A TRANSACTION
-						}  else if (operation.equals("deleteTransaction")) {
-							Statement dltStmt = conn.createStatement();
-							Statement queryStmt = conn.createStatemet();	
-
-							int transid = request.getParameter("transid");
-							ResultSet rs = queryStmt.executeQuery("SELECT userid FROM transactions WHERE transid = '" + transid + "';");
-
-							int userid = request.getParameter("userid");
-							int useridcheck = rs.getInt("userid");
-
-							int result = dltStmt.executeUpdate("DELETE FROM transactions WHERE transid = " + transid ";");	
-							if (result != 0)
-								out.print("10"); //User is the person who is owed money
-							else
-								out.print("11"); //The DELETE statement didnt execute correctly
-
-							dltStmt.close();
-							queryStmt.close();
-
-						}  else
-							out.print("2");  // COULD NOT RECOGNISE OPERATION
-
-					}
-
-				}
-
-
-
-
+}
